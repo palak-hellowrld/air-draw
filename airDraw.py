9a379e1062c892bfa226e5dbd0d645234947cc3c
Palak Mandloi <palakmandloi7@gmail.com>
2026-05-21 19:53:30 -0700

    Add requirements.txt and fix mediapipe version

--- airDraw.py
+++ airDraw.py
@@ -1,9 +1,9 @@
 import cv2
-import mediapipe as mp
+from mediapipe.python.solutions import hands as mp_hands_module
+from mediapipe.python.solutions import drawing_utils as mp_draw
 
-
-mp_hands=mp.solutions.hands
-mp_draw=mp.solutions.drawing_utils
+mp_hands = mp_hands_module
+mp_draw = mp_draw
 hands = mp_hands.Hands(static_image_mode=False, min_detection_confidence=0.3, max_num_hands=2)
 
 points=[]
